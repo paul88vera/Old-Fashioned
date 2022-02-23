9c5a870f62103fc3676e0eed3dee56145832e91d
teresacabell <teresacabll@gmail.com>
2022-02-22 19:53:03 -0600

    worked on post

--- models/User.js
+++ models/User.js
@@ -46,7 +46,7 @@ User.init(
             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
             return updatedUserData;
     }
-    {
+},
         sequelize, 
         timestamps: false, 
         freezeTableName: true, 
