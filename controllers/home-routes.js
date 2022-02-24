cfa3a25c5250ff3588c33eaf34567295f43ef084
teresacabell <teresacabll@gmail.com>
2022-02-23 18:47:24 -0600

    added some handlebars and java to api

---
+++ controllers/home-routes.js
@@ -0,0 +1,92 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+
+const { Post, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+
+  Post.findAll({
+    attributes: ["id", "ingredients", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "ingredients", "title", "post_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "ingredients", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "ingredients", "title", "post_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
