c698ba91b32b26ba06b4d7f7f8a3e511d73df891
paul88vera <paul88vera@gmail.com>
2022-02-25 22:51:51 -0600

    corrected handlebar pages

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const routes = require('./controllers');
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { engine } = require("express-handlebars");
 
-const sess = {
-    secret: 'Do not say anything',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+app.set("view engine", "handlebars");
 
-app.use(session(sess));
+app.engine("handlebars", engine({
+  layoutsDir: `${__dirname}/views/layouts`
+}));
 
-const hbs = exphbs.create({});
+app.use(express.static("public"));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine'), ('handlebars');
+app.get("/", (req, res) => {
+  res.render("main", {layout: 'index'});
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('IT IS ALIVE'));
+app.listen(port, () => {
+  console.log(`IT IS ALIVE in ${port}`);
 });
