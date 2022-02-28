b296dfd7c51bc91e90d3fb2b1da38d63cc5c0671
teresacabell <teresacabll@gmail.com>
2022-02-27 22:31:32 -0600

    added home routes

--- controllers/create-routes.js
+++ controllers/create-routes.js
@@ -1,70 +1,7 @@
 const router = require("express").Router();
-const { Post, User, Comment } = require("../models");
-const withAuth = require('../utils/auth');
 
-router.get("/", (req, res) => {
-  Post.findAll({
-    attributes: ["id", "title", "ingredients"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get("/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "title", "ingredients"],
-    include: [
-      {
-        model: Comment, 
-        attributes: ["id", "comment_text", "post_id", "user_id"],
-        include: {
-          model: User,
-          attributes: ["username"]
-        }
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
-
-router.post("/", withAuth, (req, res) => {
-  Post.create({
-    title: req.body.title, 
-    post_url: req.body.post_url, 
-    user_id: req.session.user_id,
-  })
-  .then((dbPostData) => res.json(dbPostData))
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+router.get('/', (req,res) => {
+  res.render('create');
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,4 +8,28 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/gin", (req, res) => {
+  res.render("gin", {
+    loggedIn: req.session.loggedIn
+  });
+});
+
+router.get("/vodka", (req, res) => {
+  res.render("vodka", {
+    loggedIn: req.session.loggedIn
+  });
+});
+
+router.get("/bourbon", (req, res) => {
+  res.render("bourbon", {
+    loggedIn: req.session.loggedIn
+  });
+});
+
+router.get("/tequila", (req, res) => {
+  res.render("tequila", {
+    loggedIn: req.session.loggedIn
+  });
+}); 
+
 module.exports = router;
